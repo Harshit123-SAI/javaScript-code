ecc2eca1d617d671175f54520142178ace7ecbe9
Harshit123-SAI <harshitsaini8063@gmail.com>
2025-12-22 22:24:06 +0530

    conainer cofig done

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all Js code as newer version
+
+//alert("hello") //we are using node.js no tbrowser
+
+
+console.log("Harshit")
+
+let name = "harshit"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number=> 2 to power 53
+//bright
+//string=>''
+//boolean => true/false
+//null=> standalone value
+//undefined=>
+//symbol=> unique
+
+//object
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail ="hs@gmail.com"
+var accountPassword ="1234"
+accountCity = "Delhi"
+// accountId =2
+accountEmail = "hss@gmail.com"
+accountPassword = "212222"
+accountCity = "benguluru"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
