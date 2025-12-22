4a3ad92a173381653df55eaf5f3be10d7e298e0b
Harshit123-SAI <harshitsaini8063@gmail.com>
2025-12-22 23:55:37 +0530

    conversion operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -20,7 +20,67 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//********************** Operations ********************
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Harshit"
+let str3 =  str1+str2
+//console.log(str3)
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log((3+4) * 5%3);
+
+//console.log(+true);//gives 1
+//console.log(+"");
+
+let num1, num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+//console.log(gameCounter);
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:
+
+//link to study 
+//https://tc39.es.ecma262.multipage/abstract-operations.
+html#sec-type-conversion
+
+
+
+
+
+
+
+
 
 
