8f0672cbee926306bce987665163a40a1c4a91b8
Harshit123-SAI <harshitsaini8063@gmail.com>
2025-12-22 22:48:42 +0530

    conversion of datatype

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc" //double qoutes give string
+//null=>0,undefined=>null,true=.1,false=>0,"33abc"=>NaN
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1//true, 
+// but in term of false gives 0,
+// "" =>false,
+// "abc"=> true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
